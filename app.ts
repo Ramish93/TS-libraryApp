5a88f633b6ad03f22558f36c87a4b348180373c4
Ramish Hassan <ramishshah93@gmail.com>
2022-06-28 11:25:28 +0200

    added some functions to retrive some dummy data of books

--- app.ts
+++ app.ts
@@ -1,3 +1,45 @@
-let greeting: string = 'Hello TypeScript in watch mode!';
+const getAllBooks = () => {
+    let books = [
+        {title: 'ulysses', author : 'james jouce', available: false, catagory: Catagory.fiction},
+        {title: 'a farewell', author : 'ernest', available: true, catagory: Catagory.biography},
+        {title: 'i know why', author : 'james jouce', available: true, catagory: Catagory.poetry},
+    ]
+    return books;
+}
 
-console.log(greeting);
+const logFirstAva = (books): void => {
+    let numOfBooks: number = books.length;
+    let firstAvailable = '';
+
+    for (let currentBook of books) {
+        if(currentBook.available){
+            firstAvailable = currentBook.title;
+            break
+        }
+    }
+    console.log('total', + numOfBooks);
+    console.log('first book', firstAvailable);
+    
+}
+const getBookByCatagory = (catagoryFilter: Catagory): Array<string> => {
+    console.log('catagory', Catagory[poetry]);
+    const allBooks = getAllBooks()
+    const filteredTitles: string[] = [];
+    for(let currentBook of allBooks){
+        if(currentBook.catagory === catagoryFilter) {
+            filteredTitles.push(currentBook.title)
+        }
+    }
+    return filteredTitles;
+}
+
+const logTitle = (titles: string[]): void => {
+    for(let title of titles){
+        console.log(title);
+        
+    }
+}
+
+enum Catagory {biography, poetry, fiction}
+const poetrybooks = getBookByCatagory(Catagory.poetry)
+logTitle(poetrybooks)
--- js/app.js
+++ js/app.js
@@ -1,2 +1,44 @@
-let greeting = 'Hello TypeScript in watch mode!';
-console.log(greeting);
+const getAllBooks = () => {
+    let books = [
+        { title: 'ulysses', author: 'james jouce', available: false, catagory: Catagory.fiction },
+        { title: 'a farewell', author: 'ernest', available: true, catagory: Catagory.biography },
+        { title: 'i know why', author: 'james jouce', available: true, catagory: Catagory.poetry },
+    ];
+    return books;
+};
+const logFirstAva = (books) => {
+    let numOfBooks = books.length;
+    let firstAvailable = '';
+    for (let currentBook of books) {
+        if (currentBook.available) {
+            firstAvailable = currentBook.title;
+            break;
+        }
+    }
+    console.log('total', +numOfBooks);
+    console.log('first book', firstAvailable);
+};
+const getBookByCatagory = (catagoryFilter) => {
+    console.log('catagory', Catagory[poetry]);
+    const allBooks = getAllBooks();
+    const filteredTitles = [];
+    for (let currentBook of allBooks) {
+        if (currentBook.catagory === catagoryFilter) {
+            filteredTitles.push(currentBook.title);
+        }
+    }
+    return filteredTitles;
+};
+const logTitle = (titles) => {
+    for (let title of titles) {
+        console.log(title);
+    }
+};
+var Catagory;
+(function (Catagory) {
+    Catagory[Catagory["biography"] = 0] = "biography";
+    Catagory[Catagory["poetry"] = 1] = "poetry";
+    Catagory[Catagory["fiction"] = 2] = "fiction";
+})(Catagory || (Catagory = {}));
+const poetrybooks = getBookByCatagory(Catagory.poetry);
+logTitle(poetrybooks);
